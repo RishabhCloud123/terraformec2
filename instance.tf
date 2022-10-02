0d8e303d36fc22c300e785aba66bb1a21d5873ec
RishabhCloud123 <114807543+RishabhCloud123@users.noreply.github.com>
2022-10-03 02:43:17 +0530

    Updated instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIAT4F3SXGCGEO7HX6D"
-  secret_key = "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"
+  access_key = "AKIAT4F3SXGCJH32NPFO"
+  secret_key = "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"
   region = "us-east-2"
  }
 
 resource "aws_instance" "terraformec2" {
-  ami  = "ami-0321c04d7f279eb63"
+  ami  = "ami-017ab573701202ba1"
   instance_type = "t2.micro"
   }

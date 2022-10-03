f32c23a4198c197bcee08cee83ee544e4b730785
RishabhCloud123 <114807543+RishabhCloud123@users.noreply.github.com>
2022-10-03 12:54:41 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIAT4F3SXGCJH32NPFO"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region = "us-east-2"
  }
 
 resource "aws_instance" "terraformec2" {
-  ami  = "ami-017ab573701202ba1"
+  ami  = ""
   instance_type = "t2.micro"
   }
